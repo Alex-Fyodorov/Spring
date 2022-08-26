2af17dd21bbef9a21fa6384a85fce5f9ee52573a
alexf <rassylka82@inbox.ru>
2022-08-26 19:30:05 +0300

    ДЗ-3.

--- lesson3-spring-mvc/src/main/java/fyodorov/controller/UserController.java
+++ lesson3-spring-mvc/src/main/java/fyodorov/controller/UserController.java
@@ -3,12 +3,11 @@ package fyodorov.controller;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 import fyodorov.persist.User;
 import fyodorov.persist.UserRepository;
+
+import java.util.List;
 
 @Controller
 @RequestMapping("/user")
@@ -28,9 +27,29 @@ public class UserController {
         return "user_form";
     }
 
-    @PostMapping
+    @PostMapping("/update")
     public String saveUser(User user) {
-        userRepository.update(user);
+        if (!user.getUsername().equals("")) {
+            if (user.getId().equals(0L)) {
+                userRepository.insert(new User(user.getUsername()));
+            } else {
+                userRepository.update(user);
+            }
+        }
         return "redirect:/user";
+    }
+
+    @GetMapping("/delete/{id}")
+    public String deleteUser(@PathVariable Long id) {
+        userRepository.delete(id);
+        return "redirect:/user";
+    }
+
+    @GetMapping("/new")
+    public String newUser(Model model) {
+        User user = new User("");
+        user.setId(0L);
+        model.addAttribute("user", user);
+        return "user_form";
     }
 }
